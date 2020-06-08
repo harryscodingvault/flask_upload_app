f146de597672acc66ccd72d60d4d3c82d3165143
HaroldDRF <95robots@gmail.com>
2020-06-08 16:03:30 -0700

    login/register form py completed

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin
+from flask_security.forms import RegisterForm
+from wtforms import StringField
+from flask_wtf import FlaskForm
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'mysecret'
+app.config['SECURITY_REGISTERABLE'] = True
+app.config['SECURITY_PASSWORD_SALT'] = 'somesaltfortheforum'
+app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+roles_users = db.Table('roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(250))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+    username = db.Column(db.String(255), unique=True)
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+
+class Thread(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(30))
+    description = db.Column(db.String(200))
+
+class ExtendRegisterForm(RegisterForm):
+    name = StringField('Name')
+    username = StringField('Username')
+
+class NewThread(FlaskForm):
+    name = StringField('Name')
+    description = StringField('Description')
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore, register_form=ExtendRegisterForm)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NewThread()
+    if form.validate_on_submit():
+        new_thread = Thread(title = form.data.data, description = form.description.data)
+        db.session.add(new_thread)
+        db.session.commit()
+
+    threads = Thread.query.all()
+
+    return render_template('index.html', form = form, threads = threads)
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/thread')
+def thread():
+    
+
+    return render_template('thread.html')
