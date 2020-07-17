1d13e879cd1ea2c081f8289800aac2ee28bdbd9c
HaroldDRF <95robots@gmail.com>
2020-07-16 23:45:41 -0700

    Restful api start

--- app.py
+++ app.py
@@ -1,33 +1,34 @@
-
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy 
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager 
-from flask_uploads import UploadSet, configure_uploads, IMAGES
-from flask_login import LoginManager, UserMixin, login_user, login_required, current_user, logout_user
-
 app = Flask(__name__)
 
-photos = UploadSet('photos', IMAGES)
 
-app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploads'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_photos.db'
-app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'mysecret'
+@app.route('/')
+def homepage():
+    pass
 
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 
-configure_uploads(app, photos)
+@app.route('/update_user', methods=['POST'])
+def update_user():
+    pass
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+@app.route('/login/<string:name>')
+def login(name):
+    pass
 
-from views import *
+@app.route('/logout')
+def logout(name):
+    pass
 
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
 
-if __name__ == '__main__':
-    manager.run()
-    
+
+@app.route('/album_list/')
+def create_album():
+    pass
+
+
+@app.route('/album_list/<string:album_name>')
+def check_album(album_name):
+    pass
+
+
+app.run(port=5000)
