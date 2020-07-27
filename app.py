bd3c0133114aad0eba314f98b3d0e47827b68afe
HaroldDRF <95robots@gmail.com>
2020-07-27 13:48:50 -0700

    code to adjust

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
+from marshmallow import ValidationError
+from flask_uploads import configure_uploads, patch_request_class
+from dotenv import load_dotenv
 
-from security import authenticate, identity
-from resources.user import UserRegister
-from resources.album import Album, AlbumList
-from resources.photo import Photo, PhotoList
+from db import db
+from ma import ma
+from blacklist import BLACKLIST
+from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from resources.confirmation import Confirmation, ConfirmationByUser
+from resources.image import ImageUpload, Image, AvatarUpload, Avatar
+from libs.image_helper import IMAGE_SET
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'mysecretkey'
+load_dotenv(".env", verbose=True)
+app.config.from_object("default_config")  # load default configs from default_config.py
+app.config.from_envvar(
+    "APPLICATION_SETTINGS"
+)  # override with config.py (APPLICATION_SETTINGS points to config.py)
+patch_request_class(app, 10 * 1024 * 1024)  # restrict max upload image size to 10MB
+configure_uploads(app, IMAGE_SET)
 api = Api(app)
 
 
@@ -20,17 +32,37 @@ def create_tables():
     db.create_all()
 
 
-jwt = JWT(app, authenticate, identity)  # /auth
+@app.errorhandler(ValidationError)
+def handle_marshmallow_validation(err):
+    return jsonify(err.messages), 400
 
-api.add_resource(Album, '/album/<string:name>')
-api.add_resource(AlbumList, '/album_list')
-api.add_resource(Photo, '/photo/<string:name>')
-api.add_resource(PhotoList, '/photo_list')
-api.add_resource(UserRegister, '/register')
 
-if __name__ == '__main__':
-    from db import db
+jwt = JWTManager(app)
+
+
+# This method will check if a token is blacklisted, and will be called automatically when blacklist is enabled
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token["jti"] in BLACKLIST
+
+
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(UserRegister, "/register")
+api.add_resource(User, "/user/<int:user_id>")
+api.add_resource(UserLogin, "/login")
+api.add_resource(TokenRefresh, "/refresh")
+api.add_resource(UserLogout, "/logout")
+api.add_resource(Confirmation, "/user_confirm/<string:confirmation_id>")
+api.add_resource(ConfirmationByUser, "/confirmation/user/<int:user_id>")
+api.add_resource(ImageUpload, "/upload/image")
+api.add_resource(Image, "/image/<string:filename>")
+api.add_resource(AvatarUpload, "/upload/avatar")
+api.add_resource(Avatar, "/avatar/<int:user_id>")
+
+if __name__ == "__main__":
     db.init_app(app)
+    ma.init_app(app)
     app.run(port=5000, debug=True)
-
-
